770e9f279ca6e750b178602725dabb50d0ed142f
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-05-04 21:21:18 +0500

    Add files via upload

---
+++ Qno:3.Name cases/Name_Cases.js
@@ -0,0 +1,7 @@
+"use strict";
+let personName = "Maidah Athar";
+console.log("lowercase", personName.toLowerCase());
+let PersonName = "Maidah Athar";
+console.log("upper case", personName.toUpperCase());
+let personname = "maidah ";
+console.log(PersonName.charAt(0).toUpperCase() + PersonName.slice(1).toLowerCase());
---
+++ Qno:3.Name cases/Name_Cases.ts
@@ -0,0 +1,8 @@
+let personName:string = "Maidah Athar";
+console.log("lowercase", personName.toLowerCase());
+
+let PersonName:string = "Maidah Athar";
+console.log("upper case",personName.toUpperCase());
+
+let personname:string = "maidah ";
+console.log( PersonName.charAt(0).toUpperCase()+PersonName.slice(1).toLowerCase());
